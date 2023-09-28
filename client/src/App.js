5f3bd791368c46f0151d9811977f0a2f4593aa7e
sanju6126 <sanjusv1006@gmail.com>
2023-09-28 17:12:07 +0530

    donw with the final implementation of the project...working fine..made a redux store using redux toolkit

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,16 @@ import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App">
-      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
-        <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Expense Tracker</h1>
+  <div className="App">
+    <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+      <h1 className="text-4xl py-8 mb-10 bg-[#212428] text-[#a277e2] rounded">Expense Tracker</h1>
 
-        <div className="grid md:grid-cols-2 gap-4">
+      <div className="grid md:grid-cols-2 gap-4">
           <Graph/>
           <Form/>
-        </div>
-
-
       </div>
     </div>
+  </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
